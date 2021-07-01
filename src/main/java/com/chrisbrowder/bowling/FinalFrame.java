42ebd62dab43f7e4f7950041a08df2decc7cf7ba
chrisbrowder <rcbrowder@gmail.com>
2021-07-01 11:14:16 -0400

    FinalFrame passing tests for third roll.

--- src/main/java/com/chrisbrowder/bowling/FinalFrame.java
+++ src/main/java/com/chrisbrowder/bowling/FinalFrame.java
@@ -14,7 +14,11 @@ public class FinalFrame extends Frame {
         } else if (secondRoll == null) {
             secondRoll = pins;
             score += pins;
+        } else {
+            thirdRoll = pins;
+            score += pins;
+            closed = true;
         }
-        currentGameScore = firstRoll + (secondRoll != null ? secondRoll : 0);
+        currentGameScore = firstRoll + (secondRoll != null ? secondRoll : 0) + (thirdRoll != null ? thirdRoll : 0);
     }
 }
